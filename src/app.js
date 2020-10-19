d5cefe2b0c064ee33939886ed6b834a78597dc1f
Siarhei <zykau_s@mail.ru>
2020-10-19 21:46:03 +0300

    feat: realize logging and error handling

--- src/app.js
+++ src/app.js
@@ -3,45 +3,18 @@ const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const cors = require('cors');
+const logger = require('./common/logger');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
-
-const morgan = require('morgan');
-
-process.on('uncaughtException', () => {
-  console.log('--------------------someError');
-  // process.exit(1);
-});
-
-process.on('unhandledRejection', () => {
-  console.log('--------------------Promise error');
-});
-
-setTimeout(() => {
-  throw new Error();
-}, 5000);
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  morgan((tokens, req, res) => {
-    return [
-      '\n',
-      tokens.method(req, res),
-      tokens.url(req, res).split('?')[0],
-      tokens.status(req, res),
-      '-',
-      tokens['response-time'](req, res),
-      'ms\n',
-      `query params: ${JSON.stringify(req.query)}\n`,
-      `body: ${JSON.stringify(req.body)}\n`
-    ].join(' ');
-  })
-);
+
+logger.enableLogger(app);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
@@ -57,6 +30,6 @@ app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
 
-// app.use((err, req, res, next) => {});
+logger.handleServerErrors(app);
 
 module.exports = app;
---
+++ src/common/logger.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-process-exit */
+/* eslint-disable no-unused-vars */
+
+const morgan = require('morgan');
+
+const configureMorgan = () => {
+  morgan.token('query', req => `\nquery params: ${JSON.stringify(req.query)}`);
+  morgan.token('body', req => `\nbody: ${JSON.stringify(req.body)}`);
+};
+
+const enableLogger = app => {
+  process.on('uncaughtException', err => {
+    console.error('\nUncaught Exception: ', err, '\n');
+
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (reason, promise) =>
+    console.error('\nUnhandled Rejection at:', promise, 'reason:', reason, '\n')
+  );
+
+  configureMorgan();
+  app.use(morgan('\n:method :url :status - :response-time ms :query :body\n'));
+};
+
+const handleServerErrors = app => {
+  app.use((err, req, res, next) => {
+    const { statusCode, result } = err;
+
+    if (statusCode) {
+      return res.status(err.statusCode).json({ success: false, result });
+    }
+
+    return next(err);
+  });
+
+  app.use((err, req, res, next) => {
+    console.error('\nInternal Server Error');
+
+    res.status(500).json({ success: false, err });
+  });
+};
+
+module.exports = { enableLogger, handleServerErrors };
